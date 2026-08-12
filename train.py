3dc2be6fe3ae39a2a11032efcd717b59270350b2
pier-maker92 <pierfrancesco.melucci@gmail.com>
2026-08-12 15:24:34 -0400

    testing training

--- train.py
+++ train.py
@@ -897,7 +897,7 @@ def main(cfg: DictConfig):
 
     train_dataset = TrainDatasetWrapper(
         dataset,
-        "train",
+        "test", # FIXME this is a bug
         enable_perturbed_audio=enable_perturbed_audio,
         perturbed_pitch_shift_max_semitones=perturbed_pitch_shift_max_semitones,
     )
